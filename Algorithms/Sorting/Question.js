62d782fa4feb49678bc22a835c91929ae798cde7
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-08-31 23:18:47 +0700

    sorting

---
+++ Algorithms/Sorting/Question.js
@@ -0,0 +1,23 @@
+//#1 - Sort 10 schools around your house by distance:
+// Insertion sort(very fast on small datasets and easy to code)
+
+//#2 - eBay sorts listings by the current Bid amount:
+// radix or counting sorts
+
+//#3 - Sport scores on ESPN
+// Quick sort (merge sort may be too much space complexity)
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// Merge sort (because it massive data dont want to risk O(n^2) from quick sort)
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// Insertion sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+// radix counting sort, quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// Merge sort if we have enough memory, Quick sort
+
+//#8 - You want to teach sorting for the first time
+// Bubble sort, Selection sort
